cde31ebe45c5e811b6e107ff7ddc6705f880b743
Qing0928 <73458231+Qing0928@users.noreply.github.com>
2022-08-21 00:59:44 +0800

    2022/08/21

---
+++ main.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+import aiohttp
+import asyncio
+import time
+
+conn = sqlite3.connect('kinji.db')
+sqlite = conn.cursor()
+
+start = time.time()
+
+listBook = []
+response = requests.get('http://kanji.zinbun.kyoto-u.ac.jp/db-machine/ShikoTeiyo/')
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+hrefAll = soup.find_all('a', href=True)
+for i in hrefAll:
+    url = i.get('href')
+    listBook.append(f'http://kanji.zinbun.kyoto-u.ac.jp/db-machine/ShikoTeiyo/{url}')
+
+async def SuperGiraffe(url, sem):
+    async with sem:
+        async with aiohttp.ClientSession() as response:
+            responseBody = await response.request('GET', url=url)
+            soup = BeautifulSoup(await responseBody.text(), 'lxml')
+            await response.close()
+            if soup.find('font') == None:
+                pass
+            elif len(soup.find('font').text) >= 2:
+                bookName = soup.find('font').text.replace('\n', '')
+                #print(bookName)
+                #print(f'CREATE TABLE IF NOT EXISTS {bookName}(chapter TEXT, content TEXT, url TEXT)')
+                sqlite.execute(f'CREATE TABLE IF NOT EXISTS {bookName}(chapter TEXT, content TEXT, url TEXT)')
+                contentMain = soup.find('p').text
+                chapter = soup.find('h2').text.replace('\n', '')
+                print(f'book:{bookName}, chapter:{chapter} parse')
+                if soup.find('blockquote') != None:
+                    contentExtra = soup.find('blockquote').text
+                else:
+                    contentExtra = ''
+                content = contentMain + contentExtra
+                sqlite.execute(f'INSERT INTO {bookName} (`chapter`, `content`, `url`) VALUES (\'{chapter}\', \'{content}\', \'{url}\')')
+                print(f'book:{bookName}, chapter:{chapter} done')
+                conn.commit()
+
+async def main():
+    task = []
+    sem = asyncio.Semaphore(10)
+    for i in listBook:
+        task.append(asyncio.create_task(
+            SuperGiraffe(url=i, sem=sem)
+        ))
+    await asyncio.gather(*task)
+    end = time.time()
+    print(f'{round(end-start, 3)}s')
+
+asyncio.run(main())
